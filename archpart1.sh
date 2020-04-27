1f13b8e2d843c5abe7953e5255839176c07ff2b6
bobby3605 <sonic921150@gmail.com>
2020-04-27 01:10:11 -0500

    Fixed mount order

--- archpart1.sh
+++ archpart1.sh
@@ -9,10 +9,10 @@ echo -e "g\nn\n\n\n+512M\nn\n\n\n\nw\n" | fdisk $disk
 echo -e "t\n1\n1\nw\n" | fdisk $disk
 mkfs.fat -F32 "$disk""1"
 mkfs.ext4 "$disk""2"
+mount "$disk""1" /mnt/boot
 mount "$disk""2" /mnt
 cp mirrorlist /etc/pacman.d/mirrorlist
 pacstrap /mnt base linux linux-firmware
-mount "$disk""1" /mnt/boot
 genfstab -U /mnt >> /mnt/etc/fstab
 cp mirrorlist /mnt/etc/pacman.d/mirrorlist
 cp locale.gen /mnt/etc/locale.gen
