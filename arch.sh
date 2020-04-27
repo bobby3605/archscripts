405fcad5dc9f18faa0fd192a3623f75355536471
bobby3605 <sonic921150@gmail.com>
2020-04-26 20:30:09 -0500

    Fixing more bugs

--- arch.sh
+++ arch.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 fdisk -l
-echo "\nEnter disk location, all contents will be wiped"
+echo -e "\nEnter disk location, all contents will be wiped"
 read disk
+umount "$disk""1"
+umount "$disk""2"
 # Format partitions, \n for every enter
 echo -e "g\nn\n1\n\n+512M\nt\n\1\nn\n\n\nw\n" | fdisk $disk
 mkfs.fat -F32 "$disk""1"
 mkfs.ext4 "$disk""2"
-mount "$disk""1" /mnt
+mount "$disk""2" /mnt
 cp mirrorlist /etc/pacman.d/mirrorlist
 pacstrap /mnt base linux linux-firmware
-mount "$disk""2" /mnt/boot
+mount "$disk""1" /mnt/boot
 genfstab -U /mnt >> /mnt/etc/fstab
 cp mirrorlist /mnt/etc/pacman.d/mirrorlist
 cp locale.gen /mnt/etc/locale.gen
