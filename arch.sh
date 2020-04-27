17a5954f92950ae58ebbf00e79771d50c34f9b6a
bobby3605 <sonic921150@gmail.com>
2020-04-26 20:26:09 -0500

    Fixing bugs

--- arch.sh
+++ arch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 fdisk -l
-echo "Enter disk location, all contents will be wiped"
+echo "\nEnter disk location, all contents will be wiped"
 read disk
 # Format partitions, \n for every enter
 echo -e "g\nn\n1\n\n+512M\nt\n\1\nn\n\n\nw\n" | fdisk $disk
@@ -8,12 +8,13 @@ mkfs.fat -F32 "$disk""1"
 mkfs.ext4 "$disk""2"
 mount "$disk""1" /mnt
 cp mirrorlist /etc/pacman.d/mirrorlist
-pacstrap /mnt base linux linux-firmware nano dhcpcd man grub os-prober efibootmgr git sudo
+pacstrap /mnt base linux linux-firmware
 mount "$disk""2" /mnt/boot
 genfstab -U /mnt >> /mnt/etc/fstab
 cp mirrorlist /mnt/etc/pacman.d/mirrorlist
 cp locale.gen /mnt/etc/locale.gen
 arch-chroot /mnt
+pacman -Syyu nano dhcpcd man grub os-prober efibootmgr git sudo
 systemctl enable dhcpcd
 ###ENTER TIMEZONE
 echo "Enter timezone in Region/City format"
